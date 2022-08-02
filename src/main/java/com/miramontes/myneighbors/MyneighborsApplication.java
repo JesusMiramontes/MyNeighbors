58d9cca87d5634037bf2605b98e15b735cd4fbdf
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-08-01 23:39:24 -0500

    first commit

---
+++ src/main/java/com/miramontes/myneighbors/MyneighborsApplication.java
@@ -0,0 +1,13 @@
+package com.miramontes.myneighbors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyneighborsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyneighborsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miramontes/myneighbors/MyneighborsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miramontes.myneighbors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyneighborsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
